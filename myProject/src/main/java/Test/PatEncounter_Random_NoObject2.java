9fb43d9eae9e5bb24f83469adfc780d22ce55827
matubatu <matubatu@matubatu-PC>
2016-09-18 10:42:27 +0200

    Patient-Encounter generator with unique IDs. The number of generated Resource can be changed in For-loop

---
+++ myProject/src/main/java/Test/PatEncounter_Random_NoObject2.java
@@ -0,0 +1,83 @@
+package Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+import dataGenerators.DataGenerator;
+
+public class PatEncounter_Random_NoObject2 {
+
+	public static void main(String[] args) {
+		
+		Connection con = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			
+//--- Connection-Configuration:
+			Class.forName("org.postgresql.Driver");
+			con = DriverManager.getConnection("jdbd:postgresql://localhost:2345/fhir","postgres", "");
+			if (con!=null) System.out.println("Connected");
+			st = con.createStatement();
+			st.executeUpdate(" SET plv8.start_proc = 'plv8_init' ");
+	
+//--- Patient-Encounter Creation
+			for (int i = 1; i <10; i++) { 
+				
+				String SqlPatient = " SELECT fhir_create_resource(' {"
+						+ "\"allowId\":true,"
+						+ "\"resource\":{\"resourceType\":\"Patient\","
+						+ "\"id\":\"" + i + "\", "
+						+ " \"name\":[{\"family\":\""+ DataGenerator.generateNachName() + "\", \"given\":\""+ DataGenerator.generateVorName() + "\"}], "
+						+ " \"address\":[{\"line\":[\""+ DataGenerator.generateStreet() + DataGenerator.generateHouseNumber() + "\"], "
+						+ " \"city\":\""+ DataGenerator.generateStadt() + "\", \"postalCode\":\""+ DataGenerator.generatePlz() + "\"}] "
+						+ "}} ')";	
+				
+				String SqlEncounter = " SELECT fhir_create_resource(' {"
+						+ " \"allowId\":true,"
+						+ " \"resource\":{\"resourceType\":\"Encounter\","
+						+ " \"status\":\"onleave\","
+						+ " \"patient\":{\"reference\":\"Patient/" + i + "\"} "
+						+ "}} ')";
+				
+				rs = st.executeQuery( SqlPatient );
+				rs = st.executeQuery( SqlEncounter);
+				System.out.println("processing... ");
+			} // for
+            
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(VersionTest.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+            ex.printStackTrace();
+            
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+    				System.out.println("rs closed");
+                }
+                if (st != null) {
+                    st.close();
+                    System.out.println("st closed");
+                }
+                if (con != null) {
+                    con.close();
+                    System.out.println("con closed");
+                }
+            } catch (SQLException ex) {
+                Logger lgr = Logger.getLogger(VersionTest.class.getName());
+                lgr.log(Level.WARNING, ex.getMessage(), ex);
+            }
+        }
+		
+	}// main
+
+}
