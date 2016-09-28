97a916d3ea0105c186e2b949ab2377e161856155
matubatu <matubatu@matubatu_PC>
2016-09-28 23:22:53 +0200

    last version

--- myProject/src/main/java/myProject/DataBase_AT_Name.java
+++ myProject/src/main/java/myProject/DataBase_AT_Name.java
@@ -1,6 +1,5 @@
 package myProject;
 
-import java.nio.charset.Charset;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -20,8 +19,9 @@ import dataGenerators.PLZ_StadtGenerator;
 
 public class DataBase_AT_Name {
 
-	public static final String pfad = "c:\\Users\\matubatu\\Desktop\\Map.xls";
-	public static final int locationNum = 10;
+	public static final int locationNum = 281; // max 281 items in Excel
+	public static final int patientNum = 2000;
+
 	
 	public static void main(String[] args) {
 		
@@ -36,6 +36,7 @@ public class DataBase_AT_Name {
 		try {
 			
 			//--- Connection-Configuration of DB
+
 			Class.forName("org.postgresql.Driver");
 			con = DriverManager.getConnection("jdbd:postgresql://localhost:2345/fhir","postgres", "");
 			if (con!=null) System.out.println("Server connected \n");
@@ -43,32 +44,36 @@ public class DataBase_AT_Name {
 			st.executeUpdate(" SET plv8.start_proc = 'plv8_init' ");
 	
 			//--- Location Res Creation
+
 			System.out.println("processing Location data from Excel... \n ");
 
-			// int i;
-			// for (i = 1; i <=locationNum; i++) {  // 281 items in Excel
+			int i;
+			for (i = 1; i <= locationNum; i++) {  // 281 items in Excel
 				
-			// 	String SqlLocation = " SELECT fhir_create_resource(' {"
-			// 			+ " \"allowId\":true,"
-			// 			+ " \"resource\":{\"resourceType\":\"Location\", "
-			// 			+ "\"id\":\"" + i + "\", "
-			// 			+ " \"status\":\"active\", "
-			// 			+ " \"name\":\" " + addressGenerator.getElement(ExcelElement.HOSPITAL_NAME, i) + " \", "
-			// 			+ " \"address\":[{\"line\":[\""+ addressGenerator.getElement(ExcelElement.LINE, i) + "\"], "
-			// 			+ " \"city\":\""+ addressGenerator.getElement(ExcelElement.CITY, i) + "\", \"postalCode\":\"" + addressGenerator.getElement(ExcelElement.POSTCODE, i)+ "\", \"country\":\""+ addressGenerator.getElement(ExcelElement.STATE, i) + "\"  }] "
-			// 			+ "}} ')";
+				String SqlLocation = " SELECT fhir_create_resource(' {"
+						+ " \"allowId\":true,"
+						+ " \"resource\":{\"resourceType\":\"Location\", "
+						+ "\"id\":\"" + i + "\", "
+						+ " \"status\":\"active\", "
+						+ " \"name\":\" " + addressGenerator.getElement(ExcelElement.HOSPITAL_NAME, i) + " \", "
+						+ " \"address\":[{\"line\":[\""+ addressGenerator.getElement(ExcelElement.LINE, i) + "\"], "
+						+ " \"city\":\""+ addressGenerator.getElement(ExcelElement.CITY, i) + "\", \"postalCode\":\"" + addressGenerator.getElement(ExcelElement.POSTCODE, i)+ "\", \"country\":\""+ addressGenerator.getElement(ExcelElement.STATE, i) + "\"  }] "
+						+ "}} ')";
 			
-			// 	rs = st.executeQuery( SqlLocation);
+				rs = st.executeQuery( SqlLocation);
 					
-			// } // for
-			// System.out.println(i-1 + " Location-Resources was uploaded! \n");
+			} // for
+			
+			System.out.println(i-1 + " Location-Resources was uploaded! \n");
 
 			//--- Patient-Encounter Res Creation
-			System.out.println("creating resources... \n ");
+			
+			System.out.println("creating " + patientNum + " resources... \n ");
 			
 			int j;
 			String gender="";
-			for (j = 1; j < 1000; j++) { 
+			
+			for (j = 1; j <= patientNum; j++) { 
 
 				String genderVorName = nameGenerator.randomiseGenderVorname();
 				String vorNameCut = genderVorName.substring(1);
@@ -85,48 +90,47 @@ public class DataBase_AT_Name {
 						+ " \"city\":\""+ generator.randomiseCity() + "\", \"postalCode\":\""+ generator.randomisePostcode() + "\"}] "
 						+ "}} ')";	
 				
-// 				String SqlEncounter = " SELECT fhir_create_resource(' {"
-// 						+ " \"allowId\":true,"
-// 						+ " \"resource\":{\"resourceType\":\"Encounter\","
-// 						+ "\"id\":\"" + j + "\", "
-// 						+ " \"status\":\"onleave\", "
-// 						+ " \"patient\":{\"reference\":\"Patient/" + j + "\"}, "
-// 						+ " \"location\":[{\"location\":{\"reference\":\"Location/" + DataGenerator_AT.getRandomId(locationNum) + " \"},\"status\":\"active\"}] "
-// //						+ " \"location\":[{\"location\":{\"reference\":\"Location/1 \"},\"status\":\"active\"}] "
-// 						+ "}} ')";
+ 				String SqlEncounter = " SELECT fhir_create_resource(' {"
+ 						+ " \"allowId\":true,"
+ 						+ " \"resource\":{\"resourceType\":\"Encounter\","
+ 						+ "\"id\":\"" + j + "\", "
+ 						+ " \"status\":\"onleave\", "
+ 						+ " \"patient\":{\"reference\":\"Patient/" + j + "\"}, "
+ 						+ " \"location\":[{\"location\":{\"reference\":\"Location/" + DataGenerator_AT.getRandomId(locationNum) + " \"},\"status\":\"active\"}] "
+ 						+ "}} ')";
 					
 				rs = st.executeQuery( SqlPatient );
-				// rs = st.executeQuery( SqlEncounter);
+				rs = st.executeQuery( SqlEncounter);
 					
 			} // for
-				System.out.println(j-1 + " Patient-Encoutner pair was created! \n ");
+
+			System.out.println(j-1 + " Patient-Encoutner pair was created! \n ");
             
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException ex) {
-            Logger lgr = Logger.getLogger(VersionTest.class.getName());
-            lgr.log(Level.SEVERE, ex.getMessage(), ex);
-            ex.printStackTrace();
-            
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-    				System.out.println("ResultSet closed");
-                }
-                if (st != null) {
-                    st.close();
-                    System.out.println("Statement closed");
-                }
-                if (con != null) {
-                    con.close();
-                    System.out.println("Connection closed");
-                }
-            } catch (SQLException ex) {
-                Logger lgr = Logger.getLogger(VersionTest.class.getName());
-                lgr.log(Level.WARNING, ex.getMessage(), ex);
-            }
-        }
-		
+			} catch (ClassNotFoundException e) {
+			   e.printStackTrace();
+			} catch (SQLException ex) {
+			   Logger lgr = Logger.getLogger(VersionTest.class.getName());
+			   lgr.log(Level.SEVERE, ex.getMessage(), ex);
+			   ex.printStackTrace();
+			   
+			} finally {
+			   try {
+			       if (rs != null) {
+			           rs.close();
+						System.out.println("ResultSet closed");
+			       }
+			       if (st != null) {
+			           st.close();
+			           System.out.println("Statement closed");
+			       }
+			       if (con != null) {
+			           con.close();
+			           System.out.println("Connection closed");
+			       }
+			   } catch (SQLException ex) {
+			       Logger lgr = Logger.getLogger(VersionTest.class.getName());
+			       lgr.log(Level.WARNING, ex.getMessage(), ex);
+			   }
+			}
 	}// main
 }
