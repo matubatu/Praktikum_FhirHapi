333144268ff90a85e393f74a52e52a32b50d5000
matubatu <matubatu@matubatu_PC>
2016-09-28 22:35:25 +0200

    creating search procedures as check

--- myProject/StoredProcedures/Patient_Create_Search2.sql
+++
@@ -1,5 +0,0 @@
-﻿SET plv8.start_proc = 'plv8_init';
-
-
-
-SELECT fhir_search('{"resourceType": "Patient", "queryString": "name=mann&_totalMethod=no"}');
---
+++ myProject/StoredProcedures/Patient_Create_Search_Patient.sql
@@ -0,0 +1,3 @@
+﻿SET plv8.start_proc = 'plv8_init';
+
+SELECT fhir_search('{"resourceType": "Patient", "queryString": "name=ang&_totalMethod=no"}');
